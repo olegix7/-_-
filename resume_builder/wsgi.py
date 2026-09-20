63bb6079132296910bbc0a7bf02927c6a62dae1b
olegix7 <olegzagorskij80@gmail.com>
2026-09-20 13:01:07 +0300

    Delete resume_builder directory

--- resume_builder/wsgi.py
+++
@@ -1,7 +0,0 @@
-"""WSGI config for resume_builder project."""
-
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'resume_builder.settings')
-application = get_wsgi_application()
